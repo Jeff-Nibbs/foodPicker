028ec82ce0acafc434737f786d9fdadcec436342
Jeffrey Nibbs <djcountry100@gmail.com>
2022-11-26 06:49:24 -0800

    Add broken api out of calls

--- src/App.js
+++ src/App.js
@@ -1,38 +1,58 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
-const placesToEat = [
-  "Vitality Bowls",
-  "The Cheesecake Factory",
-  "Fire Wings",
-  "Blaze Pizza",
-  "Lazy Dog",
-  "Wing Stop",
-  "El Charro Taqueria",
-  "Bibis",
-];
-
 function App() {
-  const [food, setFood] = useState("");
-  const handleClick = () => {
-    let num = Math.floor(Math.random() * placesToEat.length);
-    setFood(placesToEat[num]);
+  const [place, setPlace] = useState();
+  const [num, setNum] = useState();
+  const [input, setInput] = useState(95765);
+
+  useEffect(() => {
+    getPlaces(input);
+  }, [input]);
+
+  const getPlaces = (zip) => {
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "4a78bb8468mshe968214da6b57e9p1de614jsn8fef4e3facf0",
+        "X-RapidAPI-Host": "restaurants-near-me-usa.p.rapidapi.com",
+      },
+    };
+
+    fetch(
+      `https://restaurants-near-me-usa.p.rapidapi.com/restaurants/location/zipcode/${zip}/0`,
+      options
+    )
+      .then((response) => response.json())
+      .then((jsonData) => setPlace(jsonData.restaurants))
+      .catch((err) => console.error(err));
   };
+  const getNum = () => {
+    setNum(Math.floor(Math.random() * 10));
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    getPlaces(input);
+  };
+  const handleChange = (event) => {
+    setInput(event.target.value);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Where do you want to eat.</h1>
-        {food ? <h2>{food}</h2> : null}
-        <button className="btn" onClick={handleClick}>
+        <h2>{num ? place[num].restaurantName : null}</h2>
+
+        <form onSubmit={handleSubmit}>
+          <label>
+            Zip:
+            <input type="number" value={input} onChange={handleChange} />
+          </label>
+          <input type="submit" value="Set Zip Code" className="btn" />
+        </form>
+        <button className="btn" onClick={getNum}>
           What do you want
-        </button>
-        <button
-          className="btn"
-          onClick={() => {
-            setFood("");
-          }}
-        >
-          Clear
         </button>
       </header>
     </div>
